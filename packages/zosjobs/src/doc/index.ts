6a928ebfb82aa5875d7c540244d29aa669c158b3
Timothy Johnson <timothy.johnson@broadcom.com>
2020-07-30 14:01:32 -0400

    [ci skip] Test migration to standard monorepo structure
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

---
+++ packages/zosjobs/src/doc/index.ts
@@ -0,0 +1,34 @@
+/*
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright Contributors to the Zowe Project.
+*
+*/
+
+export * from "./input/ICancelJob";
+export * from "./input/ICancelJobParms";
+export * from "./input/ICommonJobParms";
+export * from "./input/IDeleteJobParms";
+export * from "./input/IDownloadAllSpoolContentParms";
+export * from "./input/IDownloadSpoolContentParms";
+export * from "./input/IGetJobsParms";
+export * from "./input/IMonitorJobWaitForParms";
+export * from "./input/ISubmitJclNotifyParms";
+export * from "./input/ISubmitJclParms";
+export * from "./input/ISubmitJobNotifyParms";
+export * from "./input/ISubmitJobParms";
+export * from "./input/ISubmitParms";
+
+export * from "./response/IJob";
+export * from "./response/IJobComplete";
+export * from "./response/IJobError";
+export * from "./response/IJobFeedback";
+export * from "./response/IJobFile";
+export * from "./response/IJobFileSimple";
+export * from "./response/IJobStepData";
+export * from "./response/IJobSubmit";
+export * from "./response/ISpoolFile";
